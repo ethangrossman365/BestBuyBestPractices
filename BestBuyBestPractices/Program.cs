ac853bd92410f060d703c2fe40670bdc0239f0c4
ethan grossman <ethangrossman3@gmail.com>
2023-03-29 10:08:08 -0500

    finished exercises. not bonuses.

--- BestBuyBestPractices/Program.cs
+++ BestBuyBestPractices/Program.cs
@@ -12,7 +12,8 @@ string connString = config.GetConnectionString("DefaultConnection");
 
 IDbConnection conn = new MySqlConnection(connString);
 
-var departmentRepo = new DapperDepartmentRepository(conn);
+# region Department Section
+/*var departmentRepo = new DapperDepartmentRepository(conn);
 
 departmentRepo.InsertDepartment("Ethan's New Department");
 
@@ -25,4 +26,21 @@ foreach (var department in departments)
     Console.WriteLine(department.Name);
     Console.WriteLine();
     Console.WriteLine();
+}
+*/
+#endregion
+
+var productRepository = new DapperProductRepository(conn);
+var products = productRepository.GetAllProducts();
+foreach (var product in products)
+{
+    Console.WriteLine(product.ProductID);
+    Console.WriteLine(product.Name);
+    Console.WriteLine(product.Price);
+    Console.WriteLine(product.CategoryID);
+    Console.WriteLine(product.OnSale);
+    Console.WriteLine(product.StockLevel);
+    Console.WriteLine();
+    Console.WriteLine();
+
 }
